1a76a1322155c5a7a6f26f6d1c1f79384bb1c6ab
Maddie Weber <maddieweber7@gmail.com>
2023-10-04 20:47:02 -0400

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+console.log(introduction(Aki))
+
+function introductionWithLanguage(name, language) {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+console.log(introductionWithLanguage(Aki,Ember.js))
+
+function introductionWithLanguageOptional(name, language = "Javascript") {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+    console.log(introductionWithLanguage(Gracie))
+    console.log(introductionWithLanguage(Gracie, Python))
+
